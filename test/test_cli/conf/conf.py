74bed978c898be54fcbe90db06b08a958435a7b7
quyuan <quyuan@pjlab.org>
2024-07-01 17:47:33 +0800

    update yml

---
+++ test/test_cli/conf/conf.py
@@ -0,0 +1,7 @@
+import os
+conf = {
+"code_path": os.environ.get('GITHUB_WORKSPACE'),
+"pdf_dev_path" : os.environ.get('GITHUB_WORKSPACE') + "/test/test_cli/pdf_dev",
+"pdf_res_path": "/home/quyuan/code/Magic-PDF/Magic-PDF/Magic-PDF/ci"
+
+}
